922d636d3da8688a719d427dcf8b3e942442ff2e
jsproton <johndavidsimmons@gmail.com>
2023-05-06 18:10:28 -0400

    Update url.js

--- dataelements/url.js
+++ dataelements/url.js
@@ -1,1 +1,2 @@
-`${window.location.protocol}//${window.location.host}${window.location.pathname}`
+// DE34229cc1ee654c68a4fd1eb4740a72e5
+return `${window.location.protocol}//${window.location.host}${window.location.pathname}`
